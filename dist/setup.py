89aa74050afa6bf48313beb4c7daffe16fc2996c
Harry Park <harry@harrypark.io>
2017-02-19 21:24:06 +1100

    dist + docs

---
+++ dist/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+	name = 'flask_singleview',
+	packages = ['flask_singleview'],
+	version = '0.1',
+	description = 'A flask micro extension for building single-view web apps.',
+	author = 'harryparkdotio',
+	author_email = 'harry@harrypark.io',
+	url = 'https://github.com/harryparkdotio/flask-singleview',
+	download_url = 'https://github.com/harryparkdotio/flask-singleview/archive/v0.1.tar.gz',
+	keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+	classifiers = [],
+)
