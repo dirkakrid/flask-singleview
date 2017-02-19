d8dad1be37bb3da1e135a25eb20ef5b815d8ad29
Harry Park <harry@harrypark.io>
2017-02-20 10:24:43 +1100

    license

--- dist/setup.py
+++ dist/setup.py
@@ -8,6 +8,5 @@ setup(
 	author_email = 'harry@harrypark.io',
 	url = 'https://github.com/harryparkdotio/flask-singleview',
 	download_url = 'https://github.com/harryparkdotio/flask-singleview/archive/v0.1.tar.gz',
-	keywords = ['testing', 'logging', 'example'], # arbitrary keywords
 	classifiers = [],
 )
