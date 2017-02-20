93046950a86ba806ef80ed873737d07bb48316bc
Harry Park <harry@harrypark.io>
2017-02-20 12:28:19 +1100

    more pip stuff

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
 	name = 'flask_singleview',
-	license='MIT',
 	packages = ['flask_singleview'],
+	license='MIT',
 	version = '0.1.2',
 	description = 'A flask micro extension for building single-view web apps.',
 	author = 'harryparkdotio',
 	author_email = 'harry@harrypark.io',
 	url = 'https://github.com/harryparkdotio/flask-singleview',
 	download_url = 'https://github.com/harryparkdotio/flask-singleview/archive/v0.1.tar.gz',
-	classifiers = ['Development Status :: 3 - Alpha', 'License :: OSI Approved :: MIT License', 'Intended Audience :: Developers', 'Programming Language :: Python :: 2.7',],
+	classifiers = ['Development Status :: 3 - Alpha', 'License :: OSI Approved :: MIT License', 'Intended Audience :: Developers', 'Programming Language :: Python :: 2.7'],
 	keywords='flask singleview ajax socketio',
 )
